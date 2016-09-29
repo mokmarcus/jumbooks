2c3c5d9329c0342cf6fce627a0ee5ff7fd06ce0c
mokmarcus <mokmarcus@gmail.com>
2016-09-29 17:20:18 -0400

    add book function and search my books functions added

--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,7 @@ var jumbooks_mod = angular.module('jumbooks_mod', ['ngAnimate']);
 
 /* This the "main function": it runs before everything else */
 jumbooks_mod.run(['$rootScope', '$window', function($rootScope, $window) {
-
-    $rootScope.user = {};
-
-    $window.fbAsyncInit = function() {
+    $window.fbAsyncInit = function () {
         FB.init({
             appId: '1368300386515295',
             status: true,
@@ -13,18 +10,39 @@ jumbooks_mod.run(['$rootScope', '$window', function($rootScope, $window) {
             xfbml: true,
             version: 'v2.7'
         });
+        
+        check_login_status();
+
+        // FB.Event.subscribe('auth.statusChange', function(response) {
+        //     console.log(response);
+        //     // $rootScope.$broadcast("fb_statusChange", {'status': response.status});
+        // });
     };
 
     (function(d, s, id){
         var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
+        if (d.getElementById(id)) {
+            return;
+        }
         js = d.createElement(s);
         js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js&version=v2.7";
+        js.src = "//connect.facebook.net/en_US/sdk.js";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 
 }]);
+
+function check_login_status() {
+    FB.getLoginStatus(function(response) {
+        console.log(response);
+        // if (response.status === 'connected') {
+        //     logged_in();
+        // }
+        // else {
+        //     logged_out();
+        // }
+    }, true);   
+}
 
 jumbooks_mod.controller('jumbooks_ctrl', ['$scope', '$window', '$http', function($scope, $window, $http) {
 
@@ -56,21 +74,47 @@ jumbooks_mod.controller('jumbooks_ctrl', ['$scope', '$window', '$http', function
     $scope.selected_new_book_mode = function() {
         $scope.new_book_mode = true;
         $scope.my_books_mode = false;
-    }
+    };
 
     $scope.selected_my_books_mode = function() {
         $scope.my_books_mode = true;
         $scope.new_book_mode = false;
-    }
+    };
 
     $scope.add_book_entry = function() {
         $scope.my_books_mode = true;
         $scope.new_book_mode = false;
 
-        //TODO: Add book
+        //For Testing
+        $scope.input.seller_name = "Marcus Mok";
+        $scope.input.seller_id = "12345";
 
-        $scope.input = null;
-    }
+        var book_info = "book_name=" + $scope.input.book_name
+                      + "&book_author=" + $scope.input.book_author
+                      + "&book_volume=" + $scope.input.book_volume
+                      + "&book_edition=" + $scope.input.book_edition
+                      + "&book_condition=" + $scope.input.book_condition
+                      + "&book_price=" + $scope.input.book_price
+                      + "&seller_name=" + $scope.input.seller_name
+                      + "&seller_id=" + $scope.input.seller_id
+                      + "&class_name=" + $scope.input.class_name
+                      + "&class_id=" + $scope.input.class_id
+                      + "&class_prof=" + $scope.input.class_prof;
+
+        $http({
+            method: 'POST', 
+            url: "http://localhost:8000/add",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: book_info
+        }).then(function success(response) {
+            console.log("SUCCESS");
+            $scope.input = null;
+        }, function error(response) {
+            console.log("ERROR");
+        });
+    };
 
     $scope.sell_toggle_expansion = function() {
         $scope.sell_result_expanded = !($scope.sell_result_expanded);
@@ -78,9 +122,7 @@ jumbooks_mod.controller('jumbooks_ctrl', ['$scope', '$window', '$http', function
 
     $scope.resolve_book_entry = function() {
 
-        //TODO: Remove book
-
-    }
+    };
 
     /* General functions */
 
@@ -91,6 +133,8 @@ jumbooks_mod.controller('jumbooks_ctrl', ['$scope', '$window', '$http', function
         $scope.new_book_mode = false;
         $scope.buy_result_expanded = false;
         $scope.sell_result_expanded = false;
+        $scope.search_text = null;
+        $scope.books = null;
     }
 
     $scope.back = function() {
@@ -98,30 +142,46 @@ jumbooks_mod.controller('jumbooks_ctrl', ['$scope', '$window', '$http', function
     };
 
     $scope.home = function() {
-        var landingUrl = "http://" + $window.location.host;
-        $window.location.href = landingUrl;
+        var landing_url = "http://" + $window.location.host;
+        $window.location.href = landing_url;
     };
 
     $scope.about = function() {
-        var aboutUrl = "http://" + $window.location.host + "/partial/about.html";
-        $window.location.href = aboutUrl;
+        var about_url = "http://" + $window.location.host + "/partial/about.html";
+        $window.location.href = about_url;
     };
 
     $scope.contact = function() {
-        var contactUrl = "http://" + $window.location.host + "/partial/contact.html";
-        $window.location.href = contactUrl;
+        var contact_url = "http://" + $window.location.host + "/partial/contact.html";
+        $window.location.href = contact_url;
     };
 
-    $scope.keypressEnter = function(keyEvent) {
+    $scope.key_press_enter = function(keyEvent, mode) {
         if (keyEvent.which === 13) {
-            $scope.search();
+            if ($scope.buy_clicked) {
+                $scope.search_books();
+            }
+            else if ($scope.sell_clicked && $scope.my_books_mode) {
+                $scope.search_my_books();
+            }
         }
     }
 
-    $scope.search = function() {
+    $scope.search_books = function() {
         $http({
-            method: 'GET', 
-            url: "http://localhost:8000/search?" + "book_name=" + $scope.searchText
+            method: 'GET',
+            url: "http://localhost:8000/search?" + "book_name=" + $scope.search_text
+        }).then(function success(response) {
+            $scope.books = response.data;
+        }, function error(response) {
+            console.log("ERROR");
+        });
+    };
+
+    $scope.search_my_books = function() {
+        $http({
+            method: 'GET',
+            url: "http://localhost:8000/search?" + "seller_id=" + "12345" + "&book_name=" + $scope.search_text
         }).then(function success(response) {
             $scope.books = response.data;
         }, function error(response) {
--- public/js/facebook.js
+++ public/js/facebook.js
@@ -1,32 +1,24 @@
-var fb_mod = angular.module('fb_mod', []);
-
-fb_mod.run(['$rootScope', '$window', function($rootScope, $window) {
-    console.log("init");
-    $rootScope.user = {};
-    $window.fbAsyncInit = function() {
-        FB.init({
-            appId: '1368300386515295',
-            status: true,
-            cookie: true,
-            xfbml: true,
-            version: 'v2.7'
-        });
-    };
-    (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-}]);
-
-fb_mod.controller('fb_ctrl', ['$scope', '$window', function($scope, $window) {
-	console.log("ctrl");
-    FB.login(function(response) {
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '1368300386515295',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v2.7'
+    });
+    FB.getLoginStatus(function(response) {
         if (response.status === 'connected') {
-        	console.log("logged in");
+            console.log("logged in");
+        }
+        else {
+            console.log('logged out');
         }
     });
-}]);
+};
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);         
+}(document, 'script', 'facebook-jssdk'));
