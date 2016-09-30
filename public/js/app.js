478337d4ad5ac5f38e7aa3269cf384b9fe1bdcf5
Elif Kinli <elif.kinli@tufts.edu>
2016-09-30 00:19:07 -0400

    Added delete feature in front and back end

--- public/js/app.js
+++ public/js/app.js
@@ -126,7 +126,7 @@ jumbooks_mod.controller('jumbooks_ctrl', ['$scope', '$window', '$http', function
     $scope.search_my_books = function() {
         $http({
             method: 'GET',
-            url: "http://localhost:8000/search?" + "seller_id=" + "12345" + "&book_name=" + $scope.sell_mode.my_books_mode.search_text
+            url: "http://localhost:8000/search?" + "seller_id=" + "fb_login_num" + "&book_name=" + $scope.sell_mode.my_books_mode.search_text
         }).then(function success(response) {
             $scope.sell_mode.my_books_mode.books = response.data;
         }, function error(response) {
@@ -185,6 +185,17 @@ jumbooks_mod.controller('jumbooks_ctrl', ['$scope', '$window', '$http', function
                     }
                 });
             }
+        });
+    };
+
+    $scope.resolve_book_entry = function(book_id) {
+        $http({
+            method: 'DELETE',
+            url: "http://localhost:8000/delete?book_id=" + book_id
+        }).then(function success(response) {
+            console.log(response);
+        }, function error(response) {
+            console.log("ERROR");
         });
     };
 }]);
